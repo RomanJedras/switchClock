68476cb887205f28e107b398fa97d3f68ca87989
Roman Jędras <romanjedras@o2.pl>
2019-06-23 02:56:54 +0200

    Fix this on React

--- js/script.js
+++ js/script.js
@@ -11,70 +11,70 @@ class Stopwatch extends React.Component {
 			},
 		};
 		
-		element.print(element.state.time);
+		this.print(this.state.time);
 	}
 	
-	handleOnClick(e) {
-		e.setState({
+	handleOnClick() {
+		this.setState({
 			loading: true,
 		});
 	}
 	
-	reset = (e) => {
-		e.setState ({
+	reset = () => {
+		this.setState ({
 			running: false,
 			times: {
 				minutes: 0,
 				seconds: 0,
 				miliseconds: 0
-			},
-		}),
-			e.print()
+			}
+		})
+			this.print()
 	}
 	
 	
-	print = (e) => { e.display  = this.format(this); }
+	print = () => { this.display  = this.format(this); }
 	
 	format(times) {
-		return `${this.pad0(element.state.times.minutes)}:${this.pad0(element.state.times.seconds)}:${this.pad0(Math.floor(element.state.times.miliseconds))}`;
+		return `${this.pad0(this.state.times.minutes)}:${this.pad0(this.state.times.seconds)}:${this.pad0(Math.floor(this.state.times.miliseconds))}`;
 	}
 	
-	start = (e) => {
+	start = () => {
 		
-		if(!e.state.running) {
-	  	e.state.running = true;
-		  e.watch = setInterval(() => e.step(),10);
+		if(!this.state.running) {
+	  	this.state.running = true;
+		  this.watch = setInterval(() => this.step(),10);
 		  console.log(e.watch);
 	  }
 	}
 	
-	step = (e) => {
-		if(!e.state.running);
-		e.calculate();
-		e.print(e.state.times);
+	step = () => {
+		if (!this.state.running);
+		this.calculate();
+		this.print(this.state.times);
 	}
 	
-	calculate = (e) => {
-		e.state.times.miliseconds += 1;
+	calculate = () => {
+		this.state.times.miliseconds += 1;
 		
-		if (e.state.times.miliseconds >= 100) {
-			e.state.times.seconds += 1;
-			e.state.times.miliseconds = 0;
+		if (this.state.times.miliseconds >= 100) {
+			this.state.times.seconds += 1;
+			this.state.times.miliseconds = 0;
 		}
 		
-		if (e.state.times.seconds >= 60) {
-			e.state.times.minutes += 1;
-			e.state.times.seconds = 0;
+		if (this.state.times.seconds >= 60) {
+			this.state.times.minutes += 1;
+			this.state.times.seconds = 0;
 		}
 	}
 	
-	stop = (e) => {
-		e.state.running = false;
-		clearInterval(e.watch);
+	stop = () => {
+		this.state.running = false;
+		clearInterval(this.watch);
 	}
 	
-	save = (e) => {
-		let time = this.format(e.state.times);
+	save = () => {
+		let time = this.format(this.state.times);
 		let item = document.createElement('li');
 		let itemContent = document.createTextNode(time);
 		item.appendChild(itemContent);
@@ -100,11 +100,11 @@ class Stopwatch extends React.Component {
 		return (
 			React.createElement('div', {className: 'text-center mb-2 mt-3'},
 				React.createElement('div', {className: 'controls' },
-					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2', href:'#' ,onClick: element.start}, 'start'),
-					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: element.stop}, 'stop'),
-					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: element.reset}, 'reset'),
-					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: element.save}, 'add to list'),
-					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: element.clear}, 'clear list')
+					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2', href:'#' ,onClick: this.start}, 'start'),
+					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: this.stop}, 'stop'),
+					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: this.reset}, 'reset'),
+					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: this.save}, 'add to list'),
+					React.createElement('a', {className: 'btn btn-secondary pl-2 ml-2',href:'#',onClick: this.clear}, 'clear list')
 				),
 				React.createElement('div', {id: 'stopwatch', className: "clock mt-3"}),
 				React.createElement('ul', {className: 'results'})
